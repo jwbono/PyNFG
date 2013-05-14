5e2a97e25bd6d6ab59a7ae303140c682a2f97a99
James Bono <jwbono@gmail.com>
2013-05-13 17:50:24 -0700

    release 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='PyNFG',
-    version='0.1.0',
+    version='0.1.1',
     author='James Bono',
     author_email='jwbono@gmail.com',
     packages= find_packages(),
