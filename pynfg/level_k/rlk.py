e8616f498c693ee1a69546a25cd2e3cbaa0dabcf
Justin Grana <jg3705a@student.american.edu>
2013-05-21 16:05:57 -0400

    ENH: First draft of completed relaxed level K

--- pynfg/level_k/rlk.py
+++ pynfg/level_k/rlk.py
@@ -4,7 +4,6 @@ import numpy as np
 import itertools
 
 
-
 class rlk(object):
     """ Finds the relaxed level-k solution for a semi-NFG.
 
@@ -56,12 +55,13 @@ class rlk(object):
     the one's in the game.
 
     """
-    def __init__(self, G, player_specs):
+    def __init__(self, G, player_specs, N):
         self.player_specs = player_specs
         self.G = copy.deepcopy(G)  # New attributes not permanent
         self.high_level = self._set_new_attributes()
         self._set_L0_CPT()
         self._set_satisficing_func()
+        self.N = N
 
     def _set_new_attributes(self):
         """ Sets the level and rationality of a decision node
@@ -153,13 +153,18 @@ class rlk(object):
                 return self._draw_all_mixed(nd)
         return sgen
 
-    def sample_CPT(self, node, level):
+    def sample_CPT(self, nodename, level):
         """ Samples entire CPT according to Deifnition 7 in Lee and Wolpert"""
         G = self.G
-        other_level = 'Level%s' %str(level-1)
-        for player in G.players:   # Sets all players to lower level
-            for controlled in G.partition[player]:
-                controlled.CPT = controlled.LevelCPT[other_level]
+        node = G.node_dict[nodename]
+        other_level = 'Level%s' % str(level-1)
+        for player in G.players:  # Sets all players to lower level
+            if player != node.player:
+                try:
+                    for controlled in G.partition[player]:
+                        controlled.CPT = np.copy(controlled.LevelCPT[other_level])
+                except KeyError:
+                    raise KeyError('Need to train other player\'s at level -1')
         Y = copy.copy(G.node_dict)  # Create Y
         Y.pop(node.name)
         [Y.pop(pa) for pa in node.parents.keys()]
@@ -167,43 +172,48 @@ class rlk(object):
         parent_space = []
         for par in node.parents.values():
             parent_space.append(par.space)
-            parent_combs = itertools.product(*parent_space) # Iterate through pa(node)
-        CPT_row = 0
-        trainedCPT = [ ]
-        for combo in parent_combs: # For each value of the parent
+            parent_combs = itertools.product(*parent_space)  # Iterate pa(node)
+        trained_CPT = np.zeros(node.CPT.shape)
+        for combo in parent_combs:  # For each parent
+            ix = []
+            p_node_val = zip(node.parents.keys(), combo)
+            for elem in p_node_val:
+                ix.append(G.node_dict[elem[0]].space.index(elem[1]))
+                ix = tuple(ix)  # Used to set CPT 'row' to draw value
             max_util = - np.inf
-            G.set_values(dict(zip(node.parents.keys(), combo)))  # Sets parents
-            Y_vals  = self.sample_set(Y.keys(), node.Mprime)     # STEP 2
+            G.set_values(dict(p_node_val))  # Sets parents
+            Y_vals = self.sample_set(Y.keys(), node.Mprime)     # STEP 2
             satis_set = []
             for m in range(node.M):  # STEP 1
                 sdist = node.SDist()
                 if list(sdist) not in satis_set:
                     satis_set.append((list(sdist)))
-            for sdraw in satis_set: # For each sDist
-                node.CPT[CPT_row] = sdraw
-                node.draw_value() # set CPT and draw
+            for sdraw in satis_set:  # For each SDist
+                node.CPT[ix] = sdraw
+                node.draw_value()  # set CPT and draw
                 weu = []
                 for y in Y_vals:
-                    G.set_values(y)
-                    wt = np.prod([n.prob() for n in node.parents.values()]) #STEP 2 B
+                    G.set_values(y)             # Step 2 B (below)
+                    wt = np.prod([n.prob() for n in node.parents.values()])
                     succ_samp = self.sample_set(map(lambda x: x.name,
-                                                    G.descendants(node.name)), 1)[0]
-                    G.set_values(succ_samp) # STEP 3
+                                                G.descendants(node.name)), 1)[0]
+                    G.set_values(succ_samp)  # STEP 3
                     weu.append(wt * G.utility(node.player))
                 EU = np.mean(weu)
-                if weu > max_util:
-                    max_util = weu
-                    best_strat = sdraw
+                if EU >= max_util:
+                    if EU > max_util:
+                        num_best = 1
+                        max_util = EU
+                        best_strat = sdraw
+                    if EU == max_util:
+                        best_strat = np.asarray(best_strat) * float(num_best) /\
+                            float(num_best + 1) + \
+                            np.asarray(sdraw)/float(num_best + 1)
+                        num_best += 1  # Mean update (above)
+            trained_CPT[ix] = best_strat
+        return trained_CPT
 
-
-            node.CPT[CPT_row] = best_strat
-            CPT_row +=1
-
-        return node.CPT
-
-
-
-    def sample_set(self, nodes, Mprime):
+    def sample_set(self, nodenames, Mprime):
         """ Returns a list with length Mprime
         whose elements are a dictionary of samples of nodes.
         """
@@ -211,18 +221,33 @@ class rlk(object):
         set_dicts = []
         for i in range(Mprime):
             set_samp = {}
-            for n in self.G.iterator:
-                if n.name in nodes:
-                    set_samp[n.name] = n.draw_value()
+            for n in G.iterator:
+                if n.name in nodenames:
+                    set_samp[n.name] = n.draw_value(setvalue=False)
             set_dicts.append(set_samp)
         return set_dicts
 
+    def train_node(self, nodename, level):
+        G = self.G
+        node = G.node_dict[nodename]
+        CPT = np.zeros(node.CPT.shape)
+        for mcsamp in xrange(self.N):
+            new_CPT = self.sample_CPT(nodename, level)
+            CPT = CPT * float(mcsamp)/float(mcsamp+1) + \
+                new_CPT / float(mcsamp + 1)
+        Levelkey = 'Level' + str(level)
+        node.LevelCPT[Levelkey] = CPT
 
-
-
-
-
-
+    def solve_game(self):
+        G = self.G
+        for level in np.arange(1, self.high_level):
+            for player in G.players:
+                for controlled in G.partition[player]:
+                    self.train_node(controlled.name, level)
+        for player in G.players:
+            for controlled in G.partition[player]:
+                if controlled.Level == self.high_level:
+                    self.train_node(controlled.name, self.high_level)
 
 
 
@@ -245,5 +270,3 @@ def rlk_dict(G, M=None, Mprime=None, Level=None, L0Dist=None, SDist=None):
                                             'Level': Level, 'L0Dist': L0Dist,
                                             'SDist': SDist}
     return rlk_input
-
-
