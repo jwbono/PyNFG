5596ad1b8017b0c8408baffc5e521f769a602a41
Justin Grana <jg3705a@student.american.edu>
2013-12-06 16:08:57 -0500

    Doc Changes

--- docs/conf.py
+++ docs/conf.py
@@ -131,6 +131,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
+html_sidebars = {'**' : ['localtoc.html', 'relations.html', 'sourcelink.html', 'searchbox.html','sb2.html']}
 #html_sidebars = {}
 
 # Additional templates that should be rendered to pages, maps page names to
